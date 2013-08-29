90d5a4f190e84518895a089cdf52c6871b6a37ca
Nouredine Khadri <nourredine.k@gmail.com>
2013-08-29 17:37:40 +0200

    add a genetify test mode

--- src/main/java/com/adaptivui/tapestry5/genetify/services/GenetifyModule.java
+++ src/main/java/com/adaptivui/tapestry5/genetify/services/GenetifyModule.java
@@ -16,16 +16,30 @@
 
 package com.adaptivui.tapestry5.genetify.services;
 
+import java.text.Format;
+
+import org.apache.tapestry5.MarkupWriter;
+import org.apache.tapestry5.dom.Element;
+import org.apache.tapestry5.func.Predicate;
 import org.apache.tapestry5.ioc.Configuration;
 import org.apache.tapestry5.ioc.MappedConfiguration;
 import org.apache.tapestry5.ioc.OrderedConfiguration;
 import org.apache.tapestry5.ioc.annotations.Contribute;
 import org.apache.tapestry5.ioc.annotations.Primary;
+import org.apache.tapestry5.ioc.annotations.Symbol;
+import org.apache.tapestry5.ioc.services.FactoryDefaults;
+import org.apache.tapestry5.ioc.services.SymbolProvider;
 import org.apache.tapestry5.services.ClasspathAssetAliasManager;
 import org.apache.tapestry5.services.ComponentClassResolver;
+import org.apache.tapestry5.services.Environment;
 import org.apache.tapestry5.services.LibraryMapping;
+import org.apache.tapestry5.services.MarkupRenderer;
+import org.apache.tapestry5.services.MarkupRendererFilter;
+import org.apache.tapestry5.services.RequestGlobals;
+import org.apache.tapestry5.services.javascript.InitializationPriority;
 import org.apache.tapestry5.services.javascript.JavaScriptStack;
 import org.apache.tapestry5.services.javascript.JavaScriptStackSource;
+import org.apache.tapestry5.services.javascript.JavaScriptSupport;
 import org.apache.tapestry5.services.transform.ComponentClassTransformWorker2;
 
 import com.adaptivui.tapestry5.genetify.config.GenetifyConstants;
@@ -33,39 +47,46 @@ import com.adaptivui.tapestry5.genetify.internal.GenetifyWorker;
 import com.adaptivui.tapestry5.genetify.internal.GoalWorker;
 
 /**
-* Module for tapestry-genetify
-*/
+ * Module for tapestry-genetify
+ */
 public class GenetifyModule {
-	
+
+	@Contribute(SymbolProvider.class)
+	@FactoryDefaults
+	public static void contributeSymboleProvider(MappedConfiguration<String, String> configuration)
+	{
+		configuration.add(GenetifyConstants.GENETIFY_TEST_MODE,"false");
+	}
+
 	@Contribute(ComponentClassTransformWorker2.class)
-    @Primary
-    public static void provideTransformWorkers(
-            OrderedConfiguration<ComponentClassTransformWorker2> configuration)
-    {
-        configuration.addInstance("Genetify", GenetifyWorker.class);
-        configuration.addInstance("Goal", GoalWorker.class);
-    }
-    
-    @Contribute(ClasspathAssetAliasManager.class)
-    public static void addApplicationAndTapestryMappings(MappedConfiguration<String, String> configuration)
-    {
-    	configuration.add("js", "js");
-    	configuration.add("css", "css");
-    }
-    
-    /**
+	@Primary
+	public static void provideTransformWorkers(
+			OrderedConfiguration<ComponentClassTransformWorker2> configuration)
+	{
+		configuration.addInstance("Genetify", GenetifyWorker.class);
+		configuration.addInstance("Goal", GoalWorker.class);
+	}
+
+	@Contribute(ClasspathAssetAliasManager.class)
+	public static void addApplicationAndTapestryMappings(MappedConfiguration<String, String> configuration)
+	{
+		configuration.add("js", "js");
+		configuration.add("css", "css");
+	}
+
+	/**
 	 * Contribute to the JavaScriptStackSource to provide Genetify resources
 	 * 
 	 */
-    @Contribute(JavaScriptStackSource.class)
-    public static void	addGenetifyStack(
+	@Contribute(JavaScriptStackSource.class)
+	public static void	addGenetifyStack(
 			MappedConfiguration<String, JavaScriptStack> configuration){
-	      configuration.addInstance(GenetifyConstants.GENETIFY_STACK, GenetifyStack.class);
-	    } 
-	
-    @Contribute(ComponentClassResolver.class)
-    public static void addVirtualFolder(Configuration<LibraryMapping> configuration)
-    {
-        configuration.add(new LibraryMapping("genetify", "com.adaptivui.tapestry5.genetify"));
-    }
+		configuration.addInstance(GenetifyConstants.GENETIFY_STACK, GenetifyStack.class);
+	} 
+
+	@Contribute(ComponentClassResolver.class)
+	public static void addVirtualFolder(Configuration<LibraryMapping> configuration)
+	{
+		configuration.add(new LibraryMapping("genetify", "com.adaptivui.tapestry5.genetify"));
+	}
 }

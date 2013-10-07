e4c0c11be085ff2fc8e7b97d7670cac3fa6d1f5f
Nouredine Khadri <nourredine.k@gmail.com>
2013-10-07 10:11:27 +0200

    remove useless imports

--- src/main/java/com/adaptivui/tapestry5/genetify/services/GenetifyModule.java
+++ src/main/java/com/adaptivui/tapestry5/genetify/services/GenetifyModule.java
@@ -16,30 +16,18 @@
 
 package com.adaptivui.tapestry5.genetify.services;
 
-import java.text.Format;
-
-import org.apache.tapestry5.MarkupWriter;
-import org.apache.tapestry5.dom.Element;
-import org.apache.tapestry5.func.Predicate;
 import org.apache.tapestry5.ioc.Configuration;
 import org.apache.tapestry5.ioc.MappedConfiguration;
 import org.apache.tapestry5.ioc.OrderedConfiguration;
 import org.apache.tapestry5.ioc.annotations.Contribute;
 import org.apache.tapestry5.ioc.annotations.Primary;
-import org.apache.tapestry5.ioc.annotations.Symbol;
 import org.apache.tapestry5.ioc.services.FactoryDefaults;
 import org.apache.tapestry5.ioc.services.SymbolProvider;
 import org.apache.tapestry5.services.ClasspathAssetAliasManager;
 import org.apache.tapestry5.services.ComponentClassResolver;
-import org.apache.tapestry5.services.Environment;
 import org.apache.tapestry5.services.LibraryMapping;
-import org.apache.tapestry5.services.MarkupRenderer;
-import org.apache.tapestry5.services.MarkupRendererFilter;
-import org.apache.tapestry5.services.RequestGlobals;
-import org.apache.tapestry5.services.javascript.InitializationPriority;
 import org.apache.tapestry5.services.javascript.JavaScriptStack;
 import org.apache.tapestry5.services.javascript.JavaScriptStackSource;
-import org.apache.tapestry5.services.javascript.JavaScriptSupport;
 import org.apache.tapestry5.services.transform.ComponentClassTransformWorker2;
 
 import com.adaptivui.tapestry5.genetify.config.GenetifyConstants;
